e08dac90d4ec17ee15c1bc323aafce3bdb53d035
YoungTr <tangyan1004@gmail.com>
2022-03-22 22:32:39 +0800

    signal long:setjmp

--- src/signal/sigsetjmp1.c
+++ src/signal/sigsetjmp1.c
@@ -7,36 +7,31 @@
 #include<setjmp.h>
 #include<unistd.h>
 #include<signal.h>
-typedef void (*sigfun)(int);
-sigjmp_buf jmp;
 
-void sig_alrm(int signo)
-{
-    printf("sig alrm\n");
-    siglongjmp(jmp,1);
-}
-inline void err_sys(char *str)
-{
-    printf("%s\n",str);
-    exit(-1);
+
+static void sigdel(int signo) {
+    printf("catch signal number: %d\n", signo);
 }
 
-int main()
-{
-    sigset_t oset;
-    sigprocmask(0,NULL,&oset);
-    if(sigismember(&oset,SIGALRM)) printf("SIGALRM in oset\n");
-    sigfun prefun=signal(SIGALRM,sig_alrm);
-    if(prefun==SIG_ERR) err_sys("signal(SIGALRM) error");
-    if(sigsetjmp(jmp,1)!=0){
-        sigprocmask(0,NULL,&oset);
-        if(sigismember(&oset,SIGALRM)) printf("SIGALRM in oset\n");
-        err_sys("timeout");
+int main(void) {
+    printf("pid: %d\n", getpid());
+    jmp_buf buf;
+    sigset_t newmask, oldmask, pendmask;
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGQUIT);
+    signal(SIGQUIT, sigdel);
+    signal(SIGUSR1, sigdel);
+    //阻塞SIGQUIT
+    sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+    if (sigsetjmp(buf, 1) != 0) {
+        sigpending(&pendmask);
+        if (sigismember(&pendmask, SIGQUIT)) {
+            puts("block signal exist");
+        } else {
+            puts("block signal not exist");
+        }
     }
-    int a=alarm(3);
-    printf("%d\n",a);
-    sleep(4);
-    alarm(0);
-    signal(SIGALRM,prefun);
+    sleep(8);//这期间 ctrl+\ 发送SIGQUIT
+    siglongjmp(buf, 1);
     return 0;
 }
